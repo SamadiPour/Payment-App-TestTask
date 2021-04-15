a0b3a44dcc7c02122a4115777696cc303f135554
SamadiPour <samadipoor2@gmail.com>
2021-04-16 03:33:56 +0430

    Add Fonts

--- lib/app.dart
+++ lib/app.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:payment_app_test_task/screens/transaction_screen.dart';
+import 'package:payment_app_test_task/utils/global_colors.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        textTheme: GoogleFonts.montserratTextTheme(
+          Theme.of(context).textTheme,
+        ),
+        primaryColor: GlobalColors.primary,
+      ),
       title: 'Money App',
-      theme: ThemeData(),
-      home: Scaffold(),
+      home: TransactionScreen(),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,13 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payment_app_test_task/app.dart';
 
 void main() {
+  LicenseRegistry.addLicense(() async* {
+    final license = await rootBundle.loadString('google_fonts/OFL.txt');
+    yield LicenseEntryWithLineBreaks(['google_fonts'], license);
+  });
+
   runApp(MyApp());
 }
