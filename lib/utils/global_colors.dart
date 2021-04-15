ad289dcf2febf1b4fbc6dc780567c409bb985f62
SamadiPour <samadipoor2@gmail.com>
2021-04-16 03:10:10 +0430

    Add Colors

---
+++ lib/utils/global_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class GlobalColors {
+  static const primary = Color(0XFFC0028B);
+  static const primaryLight = Color(0XFFF9E6F3);
+  static const primaryDark = Color(0XFFE4E6EB);
+  static const secondaryDark = Color(0XFFB0B3B8);
+
+  static const lightWhite = Color(0XFFF7F7F7);
+  static const background = Color(0XFF18191A);
+  static const card = Color(0XFF242526);
+  static const hover = Color(0XFF3A3B3C);
+}
